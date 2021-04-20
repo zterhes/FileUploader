19ca53fed8862834490d8d103559ed5646b20030
zterhes <zterhes1@gmail.com>
2021-04-20 18:29:35 +0200

    init backend

---
+++ fileuploader_backend/src/main/java/com/zoranterhes/fileuploader/FileuploaderApplication.java
@@ -0,0 +1,13 @@
+package com.zoranterhes.fileuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploaderApplication.class, args);
+	}
+
+}
---
+++ fileuploader_backend/src/test/java/com/zoranterhes/fileuploader/FileuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoranterhes.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
