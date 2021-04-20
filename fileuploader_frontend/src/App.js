0e3d123cbe535d4f0ec92f1f397c87cdf9e21cdd
zterhes <zterhes1@gmail.com>
2021-04-20 20:36:46 +0200

    add fileuploader to app.js

--- fileuploader_frontend/src/App.js
+++ fileuploader_frontend/src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import FileUploader from './components/FileUploader'
+import styled from 'styled-components';
+
+const Page = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+`
+
+const UploaderDiv = styled.div`
+width: 402px;
+height: 469px;
+
+`
 
 function App() {
   return (
     <div className="App">
-      <FileUploader/>
+      <Page>
+        <UploaderDiv>
+          <FileUploader />
+        </UploaderDiv>
+      </Page>
     </div>
   );
 }
--- fileuploader_frontend/src/components/FileUploader.jsx
+++ fileuploader_frontend/src/components/FileUploader.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import styled from "styled-components";
 
+const UploaderBackground = styled.div`
+height: 100%;
+width: 100%;
+background-color:white;
+border-radius: 12px;
+box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+`
+
+const Text = styled.p`
+font-size: ${props => props.textSize};
+color: #4F4F4F;
+`
 
 const FileUploader = () => {
-    return (  );
+    return (
+        <UploaderBackground>
+            <Text textSize="18px">Upload your file</Text>
+        </UploaderBackground>
+    );
 }
- 
+
 export default FileUploader;
