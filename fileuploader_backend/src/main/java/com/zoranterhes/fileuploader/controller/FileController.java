a13ddb376fcea396e24a284211a0165e6b229876
zterhes <zterhes1@gmail.com>
2021-04-21 00:16:19 +0200

    handle CORS

--- fileuploader_backend/src/main/java/com/zoranterhes/fileuploader/controller/FileController.java
+++ fileuploader_backend/src/main/java/com/zoranterhes/fileuploader/controller/FileController.java
@@ -8,15 +8,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.security.PermitAll;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/")
 public class FileController {
 
     private final UploaderService uploaderService;
