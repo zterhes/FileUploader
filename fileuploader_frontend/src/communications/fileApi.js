df500ec97f4fb74a98f77c8cc84becaa39f44335
zterhes <zterhes1@gmail.com>
2021-04-21 00:15:33 +0200

    update api call

--- fileuploader_frontend/src/communications/fileApi.js
+++ fileuploader_frontend/src/communications/fileApi.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
 
-export const uploadFile = async (files) => {
-    console.log(`files`, files)
-    await axios.post("http://localhost:8081/upload", files)
+export const uploadFile = async (file) => {
+    const fileName = file.name
+    console.log(`fileName`, fileName)
+    const formData = new FormData()
+    formData.append(`File`, file)
+    console.log(`formData`, formData)
+    await axios.post("http://localhost:8081/upload", formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
         .then(response => console.log(`response`, response))
         .catch(error => console.log(`error`, error))
 }
